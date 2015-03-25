6848ebe9b1af9a5b998742fa8342b4238c1b6f24
Stephen N. A. Sowah <niiadjei@outlook.com>
2015-03-25 14:00:45 +0000

    Building layout for app

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,6 @@
 from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
 
-'''
-Below are parameters for connecting to a MySQL DB
-when using pip install Flask-MySQL
-'''
-
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -16,28 +11,40 @@ mysql.init_app(app)
 
 
 @app.route('/')
-def index_page():
+def home():
     return render_template('home.html')
 
 
-@app.route('/Authenticate', methods=['POST', 'GET'])
-def authenticate():
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
     '''
     defining a function to connect to a mysql db and performing a login.
     '''
+
     username = request.args.get('UserName')
     password = request.args.get('Password')
     cursor = mysql.get_db().cursor()
-    data = cursor.execute("select * from ideaBox.User where username='" + username + "' and password='" + password + "'")
+    data = cursor.execute(
+        "select * from ideaBox.User where username='" + username + "' and password='" + password + "'")
     data = cursor.fetchone()
-    
+
     if data is None:
         return "Username or Password is wrong"
     else:
-        return "Logged in successfully"
+        return render_template('login.html')
 
-    
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
